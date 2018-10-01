123adb5ed5e46717ecba71d567c723c3c505741d
Alex Rozhavsky <rozalex@gmail.com>
2018-10-01 16:36:09 +0300

    daily modifications

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-"" 
+import config from './config';
+import apiRouter from './api';
+
+import express from 'express';
+const server = express();
+
+server.get('/', (req, res) => {
+	res.send("hello express");
+})
+
+server.use(express.static('public'));
+server.use('/api', apiRouter);
+
+server.listen(config.port, () => {
+	console.log('listen on ', config.port);
+});
